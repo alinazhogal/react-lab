cf3138d0e41ca102c010d0917c25cbb1954430e4
Alina <alina.zhogal.00@gmail.com>
2022-03-14 19:58:35 +0300

    feat (intro): codeowners, config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import "./styles/main.scss";
-// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
-import imgSmall from "images/testSmall.png"; // start-path is 'images' because we have an alias 'images' in webpack.common.js
 import imgCamera from "images/camera.svg";
 import { Component, StrictMode } from "react";
 import ReactDom from "react-dom";
 import style from "./styles/main.module.css";
-import someTypeScript from "./someTypeScript";
 
 interface AppProps {
   nothing: boolean;
@@ -20,9 +17,6 @@ class AppContainer extends Component<AppProps, AppState> {
 
   constructor(props: AppProps) {
     super(props);
-    this.state = {
-      title: someTypeScript("Test-block for css-modules"),
-    };
     // test class-dead-code
     const goExlcude = true;
     if (!goExlcude) {
@@ -33,12 +27,9 @@ class AppContainer extends Component<AppProps, AppState> {
   render() {
     return (
       <StrictMode>
-        <div className="test-block">
-          <h2 className={style.mainTitle}>{this.state.title}</h2>
-        </div>
+        <div className="test-block" />
         <div className={["test-block", style.background].join(" ")}>
-          <h2>Test-block for assets-module (previous url-loader)</h2>
-          <img src={imgSmall} alt="smallImage" />
+          <h2>Hello world</h2>
         </div>
         {/*  or it can be
           <img src='/src/images/testSmall.png' alt="smallImage"></img>
