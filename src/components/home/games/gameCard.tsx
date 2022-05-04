6eb9597cbfd7fd34ef0a1d0356bee6705d315fa0
Alina <alina.zhogal.00@gmail.com>
2022-05-04 14:07:39 +0300

    fix(cart): adding to cart bug

--- src/components/home/games/gameCard.tsx
+++ src/components/home/games/gameCard.tsx
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux";
 import { addCartItem } from "@/redux/actions/cartActions";
+import { useState } from "react";
 import { Game, Layout } from "./games.types";
 import "./games.scss";
-import Button from "../../../elements/button";
 import stars from "../../../assets/images/stars.svg";
 import xbox from "../../../assets/images/xbox.png";
 import pc from "../../../assets/images/desktop-computer.png";
@@ -12,11 +12,15 @@ import playstation from "../../../assets/images/playstation.png";
 function GameCard({ id, name, image, description, price, link, platforms, age, layout }: Game & { layout: Layout }) {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.auth);
+  const cart = useSelector((state: RootState) => state.cart);
+  const isAdded = cart.some((item) => item.name === name);
+  const [disabled, setDisabled] = useState(isAdded);
 
   function click(e: React.MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
     if (user.isAuth) {
       dispatch(addCartItem({ name, platforms, price, id, image }));
+      setDisabled(true);
     } else alert("not authorised");
   }
 
@@ -47,7 +51,9 @@ function GameCard({ id, name, image, description, price, link, platforms, age, l
             <div className="price-info">
               <h4>${price}</h4>
               <span>{age}+</span>
-              <Button title="Add to cart" onClick={(e) => click(e)} />
+              <button type="button" className={disabled ? "button-el disabled" : "button-el"} onClick={click}>
+                {disabled ? "Added" : "Add to cart"}
+              </button>
             </div>
           </div>
         </div>
@@ -72,7 +78,9 @@ function GameCard({ id, name, image, description, price, link, platforms, age, l
           </div>
           <div className="price-info">
             <h4>${price}</h4>
-            <Button title="Add to cart" onClick={(e) => click(e)} />
+            <button type="button" className={disabled ? "button-el disabled" : "button-el"} onClick={click}>
+              {disabled ? "Added" : "Add to cart"}
+            </button>
           </div>
         </div>
       </div>
